ecac201f032a9bcf2aef59d930674fe729e8483e
msabbaghian <46527191+msabbaghian@users.noreply.github.com>
2022-11-24 14:12:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -20,49 +20,49 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home test</h3>"
-    test = predict()
-    
-    return test
-    #return html.format(format)
+    html = "<h3>Sklearn Prediction Home</h3>"
+    return html.format(format)
 
 # TO DO:  Log out the prediction value
 @app.route("/predict", methods=['POST'])
 def predict():
-    # Performs an sklearn prediction
+    """Performs an sklearn prediction
+    input looks like:
+            {
+    "CHAS":{
+      "0":0
+    },
+    "RM":{
+      "0":6.575
+    },
+    "TAX":{
+      "0":296.0
+    },
+    "PTRATIO":{
+       "0":15.3
+    },
+    "B":{
+       "0":396.9
+    },
+    "LSTAT":{
+       "0":4.98
+    }
+    result looks like:
+    { "prediction": [ 20.35373177134412 ] }
+    """
+
     try:
-        # Load pretrained model as clf. Try any one model. 
-        # clf = joblib.load("./Housing_price_model/LinearRegression.joblib")
-        # clf = joblib.load("./Housing_price_model/StochasticGradientDescent.joblib")
-        clf = joblib.load("./Housing_price_model/GradientBoostingRegressor.joblib")
-        clf = clf[0][0]
+        clf = joblib.load("boston_housing_prediction.joblib")
     except:
         LOG.info("JSON payload: %s json_payload")
         return "Model not loaded"
-    
-    try:
-        json_payload = request.json
-    except Exception as err:
-        return f"Unexpected {err}, {type(err)}"
-    
+
+    json_payload = request.json
     LOG.info("JSON payload: %s json_payload")
-    
-    try:
-        inference_payload = pd.DataFrame(json_payload)
-        LOG.info("inference payload DataFrame: %s inference_payload")
-    except:
-        return "inference failed"
-    
-    try:
-        scaled_payload = scale(inference_payload)
-    except:
-        return "scaling failed"
-    
-    try:
-        prediction = list(clf.predict(scaled_payload))
-    except:
-        return "prediction failed"
-    
+    inference_payload = pd.DataFrame(json_payload)
+    LOG.info("inference payload DataFrame: %s inference_payload")
+    scaled_payload = scale(inference_payload)
+    prediction = list(clf.predict(scaled_payload))
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
