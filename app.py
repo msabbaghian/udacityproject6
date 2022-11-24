25a39e6099a77f28907133a4a55f55b9731d05c9
msabbaghian <46527191+msabbaghian@users.noreply.github.com>
2022-11-24 17:58:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,62 +10,57 @@ app = Flask(__name__)
 LOG = create_logger(app)
 LOG.setLevel(logging.INFO)
 
-var_test = []
-
 def scale(payload):
     """Scales Payload"""
 
     LOG.info("Scaling Payload: %s payload")
-    scaler = StandardScaler(with_mean=False).fit(payload)
+    scaler = StandardScaler().fit(payload)
     scaled_adhoc_predict = scaler.transform(payload)
     return scaled_adhoc_predict
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home v.1</h3>"
+    html = "<h3>Sklearn Prediction Home</h3>"
     return html.format(format)
 
+# TO DO:  Log out the prediction value
 @app.route("/predict", methods=['POST'])
 def predict():
-    """Performs an sklearn prediction
-    input looks like:
-            {
-    "CHAS":{
-      "0":0
-    },
-    "RM":{
-      "0":6.575
-    },
-    "TAX":{
-      "0":296.0
-    },
-    "PTRATIO":{
-       "0":15.3
-    },
-    "B":{
-       "0":396.9
-    },
-    "LSTAT":{
-       "0":4.98
-    }
-    result looks like:
-    { "prediction": [ 20.35373177134412 ] }
-    """
-
+    # Performs an sklearn prediction
     try:
-        clf = joblib.load("./boston_housing_prediction.joblib")
+        # Load pretrained model as clf. Try any one model. 
+        clf = joblib.load("./Housing_price_model/LinearRegression.joblib")
+        # clf = joblib.load("./Housing_price_model/StochasticGradientDescent.joblib")
+        # clf = joblib.load("./Housing_price_model/GradientBoostingRegressor.joblib")
         clf = clf[0][0]
     except:
         LOG.info("JSON payload: %s json_payload")
         return "Model not loaded"
-
-    json_payload = request.json
+    
+    try:
+        json_payload = request.json
+    except e:
+        return "request failed" + e
+    
     LOG.info("JSON payload: %s json_payload")
-    inference_payload = pd.DataFrame(json_payload)
-    LOG.info("inference payload DataFrame: %s inference_payload")
-    scaled_payload = scale(inference_payload)
-    prediction = list(clf.predict(scaled_payload))
+    
+    try:
+        inference_payload = pd.DataFrame(json_payload)
+        LOG.info("inference payload DataFrame: %s inference_payload")
+    except:
+        return "inference failed"
+    
+    try:
+        scaled_payload = scale(inference_payload)
+    except:
+        return "scaling failed"
+    
+    try:
+        prediction = list(clf.predict(scaled_payload))
+    except:
+        return "prediction failed"
+    
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
