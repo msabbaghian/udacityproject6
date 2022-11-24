826190d2f9e52fcc46d751eb1b46c2c00dd1caad
msabbaghian <46527191+msabbaghian@users.noreply.github.com>
2022-11-24 14:19:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,9 @@ def scale(payload):
 @app.route("/")
 def home():
     html = "<h3>Sklearn Prediction Home</h3>"
-    return html.format(format)
+    test = predict()
+    return test
+    #return html.format(format)
 
 # TO DO:  Log out the prediction value
 @app.route("/predict", methods=['POST'])
