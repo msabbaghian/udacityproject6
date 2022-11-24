be9be238db2f00cc039581acd3f81494a765d49e
msabbaghian <46527191+msabbaghian@users.noreply.github.com>
2022-11-24 16:33:15 +0100

    Linear

--- app.py
+++ app.py
@@ -30,9 +30,9 @@ def predict():
     # Performs an sklearn prediction
     try:
         # Load pretrained model as clf. Try any one model. 
-        # clf = joblib.load("./Housing_price_model/LinearRegression.joblib")
+        clf = joblib.load("./Housing_price_model/LinearRegression.joblib")
         # clf = joblib.load("./Housing_price_model/StochasticGradientDescent.joblib")
-        clf = joblib.load("./Housing_price_model/GradientBoostingRegressor.joblib")
+        # clf = joblib.load("./Housing_price_model/GradientBoostingRegressor.joblib")
         clf = clf[0][0]
     except:
         LOG.info("JSON payload: %s json_payload")
